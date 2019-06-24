e8984f3a95aaa39e200a0439ff6bc8d9488d4af8
Taron <Taron>
2019-06-24 22:50:06 +0400

    initial commit

---
+++ digitSum.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+int digitSum(int number)
+{
+    int sum = 0;
+    int tempNumber = number;
+    while(tempNumber)
+    {
+        sum += tempNumber % 10;          
+        tempNumber /= 10;
+	} 
+    
+    while(sum > 9)
+    {
+        return digitSum(sum);    
+    }
+    
+	return number; 
+}
+
+int main()
+{
+	int in;
+	std::cin >> in;
+    std::cout << digitSum(in) << std::endl;
+}
